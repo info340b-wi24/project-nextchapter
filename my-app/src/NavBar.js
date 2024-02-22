eeacaeca2eb48334edd8e1d7b6cb8ae4459f542a
briannapakk <bdpak@uw.edu>
2024-02-21 20:45:32 -0800

    try to fix navbar to connect elements

--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -1,21 +1,19 @@
 // This creates our NavBar 
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <nav>
-    <div className="project-name">
-      <Link to="/">
-          <img width="48" height="48" src="https://img.icons8.com/doodle/48/book-shelf--v1.png" alt="book-shelf--v1"/> Next Chapter
-      </Link>
+    <div class="project-name">
+       <a href="index.html">
+          <img width="48" height="48" src="https://img.icons8.com/doodle/48/book-shelf--v1.png" alt="book-shelf--v1"/> Next Chapter</a>
     </div>
     <ul>
-        <li><Link to="/UploadPage">Upload</Link></li>
-        <li><Link to="/QuizQuestion1">Quiz</Link></li>
-        <li><Link to="/AboutUs">About Us</Link></li>
-        <li><Link to="/MyShelf">My Shelf</Link></li>
+       <li><a href="uploadpage.html">Upload</a></li>
+       <li><a href="quizquestion1.html">Quiz</a></li>
+       <li><a href="aboutus.js">About Us</a></li>
+       <li><a href="myshelf.html">My Shelf</a></li>
     </ul>
  </nav>
   );
