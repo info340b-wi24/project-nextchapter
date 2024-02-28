262184b65e9668f4ef7b8eb144a0d917a1f23c9f
briannapakk <bdpak@uw.edu>
2024-02-28 14:10:22 -0800

    linked myshelf to home page

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import AboutUs from './AboutUs';
-import Home from './HomePage';
+import Home from './Homepage';
 import UploadForm from './UploadPage'; // Make sure to adjust the path as necessary
 import BookList from './BookCard'; // Make sure to adjust the path as necessary
 import Footer from './Footer';
